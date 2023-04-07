770efe7cab4fb0e862f431a31d2287da423f59c6
Roger Burrows <90137418+myrburrows@users.noreply.github.com>
2023-04-07 09:21:11 -0500

    Add MYWORD list of possible words

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 import { WORDS } from "./words.js";
+import { MYWORD } from "./myword.js";
 
 const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
+let rightGuessString = MYWORD[Math.floor(Math.random() * MYWORD.length)];
 
 console.log(rightGuessString);
 
